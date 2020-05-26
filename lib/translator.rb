18bb4420f86f05bbd3414a7c2ffa0f4a1de61991
Bree Warren <ambreea.warren@gmail.com>
2020-05-26 23:39:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,51 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+library = YAML.load_file("lib/emoticons.yml")
+revised_library = {}
+library.each do |key, value|
+  revised_library = key[:english]
+ revised_library << "#{[expression]}: english: #{translation[0]}, japanese: #{translation[1]} "
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+#frank_emo[name] ||= {}
+#frank_emo[name][:english] = arrays[0]
+#frank_emo[name][:japanese] = arrays[1]
 
-def get_english_meaning
+
+
+#def get_japanese_emoticon
   # code goes here
-end
+#end
+
+#def get_english_meaning
+  # code goes here
+#end
+
+
+ #code goes here
+ # emo = YAML.load_file("lib/emoticons.yml")
+  #frank_emo = {}
+  #emo.each do |name,arrays|
+   #   frank_emo[name] ||= {}
+    #  frank_emo[name][:english] = arrays[0]
+     # frank_emo[name][:japanese] = arrays[1]
+#  end
+#frank_emo
+#end
+
+
+#countries_and_capitals_of_the_world = {
+ # :north_america => {
+  #  "Canada" => {
+   #   :capital => "Ottawa",
+    #  :capital_climate => "Kőppen Dfb"
+  #  },
+   # "USA" => {
+    #  :capital => "Washington D.C.",
+     # :capital_climate => "Kőppen Cfa"
+  #  }
+  #},
+  #:africa => {
