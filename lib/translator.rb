6652d73da02fbabb6e05da544b881ae6d6124847
Bree Warren <ambreea.warren@gmail.com>
2020-05-27 00:33:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,21 +5,14 @@ def load_library(file_path)
 library = YAML.load_file("lib/emoticons.yml")
 final_result = {}
 library.each do |key, value|
-  final_result[:english][value[1]] = key
-  final_result[:japanese][value[0]] = value[1]
+  final_result[key] || final_result[key] = {}
+  final_result[key][:english] = value[0]
+  final_result[key][:japanese] = value[2]
 end
 final_result
 end
 
 
-  frank_emo = {}
-  emo.each do |name,arrays|
-      frank_emo[name] || frank_emo[name] = {}
-      frank_emo[name][:english] = arrays[0]
-      frank_emo[name][:japanese] = arrays[1]
-  end
-frank_emo
-end
 
 #frank_emo[name] || = {}
 #frank_emo[name][:english] = arrays[0]
