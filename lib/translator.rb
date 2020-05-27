9dd9b798f1780e1d294ba553dc6e9d0dab20b6a8
Bree Warren <ambreea.warren@gmail.com>
2020-05-27 00:37:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,43 +12,10 @@ end
 final_result
 end
 
-
-
-#frank_emo[name] || = {}
-#frank_emo[name][:english] = arrays[0]
-#frank_emo[name][:japanese] = arrays[1]
-
-
-#def get_japanese_emoticon
+def get_japanese_emoticon
   # code goes here
-#end
+end
 
 #def get_english_meaning
   # code goes here
 #end
-
-
- #code goes here
- # emo = YAML.load_file("lib/emoticons.yml")
-  #frank_emo = {}
-  #emo.each do |name,arrays|
-   #   frank_emo[name] ||= {}
-    #  frank_emo[name][:english] = arrays[0]
-     # frank_emo[name][:japanese] = arrays[1]
-#  end
-#frank_emo
-#end
-
-
-#countries_and_capitals_of_the_world = {
- # :north_america => {
-  #  "Canada" => {
-   #   :capital => "Ottawa",
-    #  :capital_climate => "Kőppen Dfb"
-  #  },
-   # "USA" => {
-    #  :capital => "Washington D.C.",
-     # :capital_climate => "Kőppen Cfa"
-  #  }
-  #},
-  #:africa => {
