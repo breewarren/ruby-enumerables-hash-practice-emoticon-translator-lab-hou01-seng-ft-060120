88632515b5279f3bac6f8fb9bbbb4b91e5dec995
Bree Warren <ambreea.warren@gmail.com>
2020-05-27 00:00:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,6 @@ library = YAML.load_file("lib/emoticons.yml")
 revised_library = {}
 library.each do |key, value|
   revised_library[key][:english] = value[0]
-  binding.pry
   revised_library[key][:japanese] = value[1]
  revised_library << "#{[expression]}: english: #{translation[0]}, japanese: #{translation[1]} "
   end
