ad037b2f938b6dfeb3439955e30f1bb33b08dcac
Bree Warren <ambreea.warren@gmail.com>
2020-05-27 01:04:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,10 @@ def get_english_meaning(file_path, emoticon)
   library.each do |key, value|
     if value[:japanese] == emoticon
     return key
+    else
+      
   end
-  end
+end
     "Sorry, that emoticon was not found"
 end
 
