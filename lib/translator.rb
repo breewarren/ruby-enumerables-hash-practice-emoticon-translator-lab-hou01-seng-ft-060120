f032dde5ec29aaf72765fd04ddf08b6c2da3674d
Bree Warren <ambreea.warren@gmail.com>
2020-05-27 00:48:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ final_result
 end
 
 def get_english_meaning(file_path, emoticon)
-  library = load_library(file_path)
+  library = load_library("lib/emoticons.yml")
   library.each do |key, value|
     if value[:japanese] == emoticon
       key
@@ -23,4 +23,41 @@ def get_english_meaning(file_path, emoticon)
   end
 end
   
+  
+  
+   # code goes here
+end
+
+def get_english_meaning(file_path, jap_emo)
+  # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+    return meaning if idioms[:japanese] == jap_emo
+  end
+    "Sorry, that emoticon was not found"
+ end 
+
+
+def get_japanese_emoticon
+  # code goes here
+end
+
+
+def get_japanese_emoticon(file_path, eng_emo)
+  # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+      return idioms[:japanese] if idioms[:english] == eng_emo
+  end
+  "Sorry, that emoticon was not found"
+end
+
+def get_english_meaning(file_path, jap_emo)
+  # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+    return meaning if idioms[:japanese] == jap_emo
+  end
+    "Sorry, that emoticon was not found"
+ end 
  
